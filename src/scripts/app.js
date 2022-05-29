5db1131a2d1fbc8651354f6005a5a9dbb2459d17
Radek <radek42310o@gmail.com>
2022-05-29 17:57:50 +0200

    add notification for guessed color and scoring points

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,33 +7,43 @@ const drawnColorText = document.querySelector('.game-dashboard__color-name > spa
 const changeCurrentColorOfElements = document.querySelector('.game-dashboard__randomize-colors');
 const selectColorSystem = document.querySelector('.game-dashboard__change-color-system');
 const submitAnswerButton = document.querySelector('.game-dashboard__submit-answer');
+const correctAnswersAmount = document.querySelector('.statistics-dashboard__info_correct');
+const finalPoints = document.querySelector('.statistics-dashboard__info_points');
+const badAnswersAmount = document.querySelector('.statistics-dashboard__info_incorrect');
+const correctColorNotification = document.querySelector('.drawn-color-pop-up');
 const randomizeRgbColorValues = () => {
     const rValueFromRGB = Math.round(Math.random() * 255);
     const gValueFromRGB = Math.round(Math.random() * 255);
     const bValueFromRGB = Math.round(Math.random() * 255);
     return `rgb(${rValueFromRGB}, ${gValueFromRGB}, ${bValueFromRGB})`;
 };
+const returnElementsToInitialState = () => {
+    submitAnswerButton.removeAttribute('disabled', '')
+    colorElements.forEach((el) => {
+        el.firstElementChild.textContent = 'Reveal color';
+        el.removeAttribute('disabled', '');
+        el.firstElementChild.checked = false;
+    });
+    colorCaptions.forEach((el) => {
+        el.classList.remove('revealed');
+        el.textContent = 'Reveal color';
+        el.classList.remove('revealed');
+    });
+};
 
 const renderElementsWithRandomColors = () => {
     colorElements.forEach((el) => {
         el.style.backgroundColor = `${randomizeRgbColorValues()}`;
-        el.firstElementChild.textContent = 'Reveal color';
-        el.firstElementChild.classList.remove('revealed');
-        console.log(Math.round(Math.random() * colorElements.length));
-        el.removeAttribute('disabled', '');
-        el.firstElementChild.checked = false;
     });
-    colorCaptions.forEach(el => {
-        el.classList.remove('revealed')
-        el.textContent = 'Reveal color';
-    })
-    
-    drawnColorText.textContent = colorElements[Math.round(Math.random() * colorElements.length)].style.backgroundColor;
+    drawnColorText.textContent = colorElements[Math.round(Math.random() * (colorElements.length - 1))].style.backgroundColor;
 };
 
 window.addEventListener('DOMContentLoaded', renderElementsWithRandomColors);
 
-changeCurrentColorOfElements.addEventListener('click', renderElementsWithRandomColors);
+changeCurrentColorOfElements.addEventListener('click', () => {
+    returnElementsToInitialState()
+    renderElementsWithRandomColors();
+});
 colorCaptions.forEach((captionEl) => {
     captionEl.addEventListener('click', () => {
         captionEl.classList.add('revealed');
@@ -42,36 +52,29 @@ colorCaptions.forEach((captionEl) => {
     });
 });
 
-// selectColorSystem.addEventListener('blur', () => {
-//     const currectDrawnColor = drawnColorText.textContent;
-//     console.log(currectDrawnColor);
-//     switch (selectColorSystem.value) {
-//         case 'RGB':
-//             drawnColorText.textContent = Color(currectDrawnColor).rgb();
-//             console.log(drawnColorText);
-//             break;
-//         case 'HEX':
-//             drawnColorText.textContent = Color(currectDrawnColor).hex();
-//             console.log(drawnColorText);
-//             break;
-//         case 'HSL':
-//             drawnColorText.textContent = Color(currectDrawnColor).hsl();
-//             console.log(drawnColorText);
-//             break;
-//     }
-// });
-
-const validateAnswerFromUser = () => {
+const validateAnswerFromUser = (button) => {
     colorElements.forEach((colorElement) => {
         const checkboxState = colorElement.firstElementChild.checked;
         const currentBackgroundValue = colorElement.style.backgroundColor;
         if (checkboxState && currentBackgroundValue === drawnColorText.textContent) {
-            console.log(currentBackgroundValue === drawnColorText.textContent)
-            console.log('you guessed that !');
+            correctAnswersAmount.value++;
+            submitAnswerButton.setAttribute('disabled', '');
+            correctColorNotification.classList.add('active')
+            setTimeout(() => {
+                correctColorNotification.classList.remove('active')
+            }, 4000)
+            correctColorNotification.addEventListener('click', () => {
+                correctColorNotification.classList.remove('active')
+            })
         } else if (checkboxState && currentBackgroundValue !== drawnColorText) {
-            colorElement.setAttribute('disabled', '')
+            colorElement.firstElementChild.checked = false
+            badAnswersAmount.value++
+            colorElement.setAttribute('disabled', '');
         }
     });
 };
 
-submitAnswerButton.addEventListener('click', validateAnswerFromUser);
+submitAnswerButton.addEventListener('click', ({ target }) => {
+    console.log(target)
+    validateAnswerFromUser(target);
+});
